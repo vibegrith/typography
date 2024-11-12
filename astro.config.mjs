f98b4343d432693046471439b45d0f3372ce94de
vibegrith <vibe9737@stud.kea.dk>
2024-11-12 19:57:40 +0100

    public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: "dist",
   integrations: [tailwind(), alpinejs()],
 });
